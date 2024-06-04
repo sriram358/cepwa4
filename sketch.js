1a338fb06ae7746dca42d31ce142408379407825
vs358 <sriramvenky24@gmail.com>
2024-06-04 12:52:05 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let player1body, player1head, player1, player2body, player2head, player2, floor, ampl
+let player1body, player1head, player1, player2body, player2head, player2, floor, ampl, collisionFrame
 
 //let frameCount = 0;
 let changeFrame = -100;
@@ -8,10 +8,10 @@ let rotateDire2 = "right"
 function setup(){
     createCanvas(1200, 800)
     angleMode(DEGREES)
-    player1head = new Sprite(200, 400, 100, 300)
-    player2head = new Sprite(800, 400, 100, 300)
-    player1body = new Sprite(200, 550, 100)
-    player2body = new Sprite(800, 550, 100)
+    player1head = new Sprite(200, 600, 100, 300)
+    player2head = new Sprite(800, 600, 100, 300)
+    player1body = new Sprite(200, 750, 100)
+    player2body = new Sprite(800, 750, 100)
 
     player1 = new GlueJoint(player1head, player1body)
     player2 = new GlueJoint(player2head, player2body)
@@ -27,12 +27,14 @@ function setup(){
     player1body.rotationDrag = 10
     player2body.rotationDrag = 10
     ampl = 10
+    collisionFrame = 0
 }
 
 function draw(){
     background(200)
    
     if(player1body.colliding(floor)){
+        collisionFrame += 1;
         // if(player1body.rotation > 0){
         //     if(frameCount - changeFrame > 30){
         //         player1head.rotateTowards(200, 400, 0.01, 0)
@@ -51,17 +53,17 @@ function draw(){
             
         // }
         if(ampl > 0){
-            ampl -= 0.1
+            ampl -= 0.03
         } else {
             ampl = 0
         }
         
-        player1head.rotation = 5*ampl*Math.cos(frameCount/60)
+        player1head.rotation = 5*ampl*Math.cos(collisionFrame/30)
     }
 
     if(kb.presses('w')){
-        player1head.position.y += 10
-        player1head.position.x += Math.cos(player1head.rotation)
+        player1head.velocity.y -= 10
+        player1head.velocity.x += 10*Math.sin(radians(player1head.rotation))
     }
     //player1head.rotateTowards(200, 400, 0.1, 0)
     
