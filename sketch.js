dbfa6df457e91c4adbe2661e1da62fe76744c2ef
vs358 <sriramvenky24@gmail.com>
2024-06-10 00:49:57 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,45 @@
-let player1body, player1head, player1, player2body, player2head, player2, floor, ampl1, ampl2, collisionFrame1, collisionFrame2, lastCollide1, lastCollide2, initRotation1, initRotation2, wall1, wall2
+let player1body, player1head, player1, player2body, player2head, player2, floor, ampl1, ampl2, collisionFrame1, collisionFrame2, lastCollide1, lastCollide2, initRotation1, initRotation2, wall1, wall2, table
 
+let backarm1, forearm1, armjoint1, bodyjoint1, armjoint2
 //let frameCount = 0;
 let changeFrame = -100;
 let rotateDire1 = "left"
 let rotateDire2 = "right"
-
+let armRotation1 = 180
+let armRotation2 = 180
 function setup(){
     createCanvas(1200, 800)
     angleMode(DEGREES)
    
-    player1 = new Sprite(200, 550, 100)
-    player2 = new Sprite(800, 550, 100)
+    player1 = new Sprite(100, 550, 50)
+    player2 = new Sprite(1000, 550, 50)
     floor = new Sprite(600, 800, 1200, 4)
     wall1 = new Sprite(0, 400, 4, 800)
     wall2 = new Sprite(1200, 400, 4, 800)
-    floor.collider = "static"
+    table = new Sprite(600, 750, 500, 150)
+    
+    
+    backarm1 = new Sprite(100, 400, 30, 100)
+    backarm1.offset.y = -50
+    forearm1 = new Sprite(100, 480, 30, 100)
+    forearm1.offset.y = 50
+    backarm1.collider = "kinematic"
+    forearm1.collider = "kinematic"
+    //bodyjoint1 = new GlueJoint(backarm1, player1)
+    //armjoint1 = new GlueJoint(backarm1, forearm1)
+    floor.collider = "kinematic"
     wall1.collider = "static"
     wall2.collider = "static"
+    table.collider = "static"
+    player1.collider = "dynamic"
+    player2.collider = "dynamic"
+    backarm1.rotation = 180
+
     floor.visible = false
     wall1.visible = false
     wall2.visible = false
-    player1.d = 100
-    player2.d = 100
+    player1.d = 50
+    player2.d = 50
     player1.rotation = 0
     player2.rotation = 0
     player1.bounciness = 0
@@ -32,6 +50,10 @@ function setup(){
     player2.friction = 1000
     player1.visible = false
     player2.visible = false
+    player1.rotationDrag = 100
+    player2.rotationDrag = 100
+    // player1.addCollider(0,-125, 50, 200)
+    // player2.addCollider(0,-125, 50, 200)
     floor.friction = 100
     world.gravity.y = 20
     ampl1 = 1
@@ -47,8 +69,8 @@ function drawPlayer1(){
     translate(player1.pos.x, player1.pos.y)
     rotate(player1.rotation)
     fill(255, 0, 0)
-    circle(0, 0, 100)
-    rect(-50, -50-300, 100, 300)
+    circle(0, 0, 50)
+    rect(-25, -25-200, 50, 200)
     pop()
 }
 
@@ -58,16 +80,51 @@ function drawPlayer2(){
     translate(player2.pos.x, player2.pos.y)
     rotate(player2.rotation)
     fill(0, 0, 255)
-    circle(0, 0, 100)
-    rect(-50, -50-300, 100, 300)
+    circle(0, 0, 50)
+    rect(-25, -25-200, 50, 200)
     pop()
+}
+
+function renderArm1(){
+    backarm1.pos.x = player1.pos.x + 180*Math.sin(radians(player1.rotation))
+    backarm1.pos.y = player1.pos.y - 180*Math.cos(radians(player1.rotation))
 }
 
 function draw(){
     background(200)
     drawPlayer1()
     drawPlayer2()
+    renderArm1()
     // Fixes bounce bug in planck (inherent of p5play sadly)
+
+    if(kb.pressing('w')){
+        if(armRotation1 > 0 + player1.rotation){
+            armRotation1 = max(0 + player1.rotation, armRotation1 - 10)
+           //wwbackarm1.rotation -= 5
+        } else {
+            armRotation1 = armRotation1
+        }
+
+        backarm1.rotation = armRotation1
+
+    } else {
+        
+        
+        if(armRotation1 < 180 + player1.rotation){
+            armRotation1 = min(180 + player1.rotation, armRotation1 + 10)
+            //armRotation1 += 5
+        } else {
+            armRotation1 = 180 + player1.rotation
+        }
+
+        backarm1.rotation = armRotation1
+        
+        
+    }
+
+    console.log(armRotation1)
+    
+    
     
     if(player1.colliding(floor)){
         lastCollide1 = frameCount
@@ -88,7 +145,10 @@ function draw(){
         }
         
         
+        
     } else {
+        player1.velocity.y += 0.1
+    
        
         if(frameCount - lastCollide1 > 2){
             player1.rotationLock = false
@@ -122,7 +182,7 @@ function draw(){
             player2.velocity.x += 8*Math.sin(radians(player2.rotation))
         }
     } else {
-        
+        player2.velocity.y += 0.1
         if(frameCount - lastCollide2 > 2){
             player2.rotationLock = false
             if(player2.rotation < 0){
