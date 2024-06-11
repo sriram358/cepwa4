65c2def73aa81aa08414de22f4d5569ec7ff372c
vs358 <sriramvenky24@gmail.com>
2024-06-11 12:59:02 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,21 +16,24 @@ function setup(){
     floor = new Sprite(600, 800, 1200, 4)
     wall1 = new Sprite(0, 400, 4, 800)
     wall2 = new Sprite(1200, 400, 4, 800)
-    table = new Sprite(600, 800, 500, 150)
-    net = new Sprite(600, 700, 20, 50)
+    table = new Sprite(600, 750, 500, 150)
+    net = new Sprite(600, 660, 20, 30)
     ball = new Sprite(800, 500, 20)
     
     
-    backarm1 = new Sprite(100, 400, 30, 100)
+    backarm1 = new Sprite(100, 400, 30, 120)
     backarm1.offset.y = -50
-    backarm2 = new Sprite(1000, 400, 30, 100)
+    backarm2 = new Sprite(1000, 400, 30, 120)
     backarm2.offset.y = -50
-    forearm1 = new Sprite(100, 480, 30, 100)
-    forearm1.offset.y = 50
+    
     ball.collider = "dynamic"
     backarm1.collider = "kinematic"
     backarm2.collider = "kinematic"
-    forearm1.collider = "kinematic"
+    backarm1.mass = 60
+    backarm2.mass = 60
+    player1.mass = 1000
+    player2.mass = 1000
+    
     net.collider = "static"
     //bodyjoint1 = new GlueJoint(backarm1, player1)
     //armjoint1 = new GlueJoint(backarm1, forearm1)
@@ -63,15 +66,17 @@ function setup(){
     // player1.addCollider(0,-125, 50, 200)
     // player2.addCollider(0,-125, 50, 200)
     floor.friction = 100
-    world.gravity.y = 20
+    world.gravity.y = 25
     ampl1 = 1
     ampl2 = 1
     collisionFrame1 = 0
     collisionFrame2 = 0
     lastCollide1 = 0
     lastCollide2 = 0
-    ball.velocity.x = -10
-    ball.bounciness = 0.9
+    ball.velocity.x = -15
+    ball.bounciness = 0.8
+    ball.mass = 600
+    ball.drag = 0.5 
 }
 
 function drawPlayer1(){
@@ -114,80 +119,144 @@ function draw(){
     // Fixes bounce bug in planck (inherent of p5play sadly)
 
     
-    if(ball.colliding(backarm1)){
-        ball.vel.x = 10
-    } 
+    // if(ball.colliding(backarm1)){
+    //     ball.vel.x = 10
+    // } 
     
-    if(ball.colliding(backarm2)){
-        ball.vel.x = -10
-    }
-    
+    // if(ball.colliding(backarm2)){
+    //     ball.vel.x = -10
+    // }
 
-    if(kb.pressing('w')){
-        if(player1.pos.y > 760){
-            player1.pos.y -= 2
-            player1.velocity.y -= 2
-            player1.velocity.x += 2*Math.sin(radians(player1.rotation))
-        }
+    ball.vel.limit(20)
+    
+    console.log(backarm1.rotation, player1.rotation)
+
+    if(kb.pressing('e')){
         if(armRotation1 > 0 + player1.rotation){
-            armRotation1 = max(0 + player1.rotation, armRotation1 - 10)
-           //wwbackarm1.rotation -= 5
+            backarm1.rotate(-min(10, Math.abs(armRotation1 - player1.rotation)), 10)
+            if(backarm1.rotation <= -90){
+                armRotation1 = backarm1.rotation + 360
+            } else {
+                armRotation1 = backarm1.rotation
+            }
+            
+            
         } else {
-            armRotation1 = armRotation1
-        }
-
-        //backarm1.rotateTo((armRotation1%360)-180, 1) 
-        backarm1.rotation = armRotation1
-
+            if(backarm1.rotation <= -90){
+                armRotation1 = backarm1.rotation + 360
+            } else {
+                armRotation1 = backarm1.rotation
+            }
+            armRotation1 = player1.rotation
+            backarm1.rotation = armRotation1
+        } 
+        
     } else {
-        
-        
         if(armRotation1 < 180 + player1.rotation){
-            armRotation1 = min(180 + player1.rotation, armRotation1 + 10)
-            //armRotation1 += 5
+            
+            backarm1.rotate(min(10, Math.abs(armRotation1 - (180 + player1.rotation))), 10)
+            if(backarm1.rotation <= -90){
+                armRotation1 = backarm1.rotation + 360
+            } else {
+                armRotation1 = backarm1.rotation
+            }
         } else {
             armRotation1 = 180 + player1.rotation
+            backarm1.rotation = armRotation1
+            if(backarm1.rotation <= -90){
+                armRotation1 = backarm1.rotation + 360
+            } else {
+                armRotation1 = backarm1.rotation
+            }
+        } 
+    }
+    if(kb.pressing('w')){
+        if(player1.pos.y > 750 && kb.pressing('w') < 40){
+            player1.pos.y -= 2 
+            player1.velocity.y -= 3
+            player1.velocity.x += 2*Math.sin(radians(player1.rotation))
         }
 
-        //backarm1.rotateTo((armRotation1%360)-180, 1) 
-        backarm1.rotation = armRotation1
         
-        
+        //backarm1.rotation = armRotation1
+
+    } 
+
+    if(kb.pressing('i')){
+        if(armRotation2 < 360 + player2.rotation){
+            
+            backarm2.rotate(min(10, Math.abs(armRotation2 - (360 + player2.rotation))), 10)
+            if(backarm2.rotation <= 90){
+                armRotation2 = backarm2.rotation + 360
+            } else {
+                armRotation2 = backarm2.rotation
+            }
+        } else {
+            armRotation2 = 360 + player2.rotation
+            backarm2.rotation = armRotation2
+            if(backarm2.rotation <= 90){
+                armRotation2 = backarm2.rotation + 360
+            } else {
+                armRotation2 = backarm2.rotation
+            }
+        } 
+
+        //backarm2.rotation = armRotation2
+    } else {
+        if(armRotation2 > 180 + player2.rotation){
+            backarm2.rotate(-min(10, Math.abs(armRotation2 - (180 + player2.rotation))), 10)
+            if(backarm2.rotation <= 90){
+                armRotation2 = backarm2.rotation + 360
+            } else {
+                armRotation2 = backarm2.rotation
+            }
+            
+            
+        } else {
+            if(backarm2.rotation <= 90){
+                armRotation2 = backarm2.rotation + 360
+            } else {
+                armRotation2 = backarm2.rotation
+            }
+            armRotation2 = player2.rotation + 180
+            backarm2.rotation = armRotation2
+        } 
     }
 
     if(kb.pressing('o')){
-        if(player2.pos.y > 760){
+        if(player2.pos.y > 750 && kb.pressing('o') < 40){
             player2.pos.y -= 2
-            player2.velocity.y -= 2
+            player2.velocity.y -= 3
             player2.velocity.x += 2*Math.sin(radians(player2.rotation))
         }
 
-        if(armRotation2 < 360 + player2.rotation){
-            armRotation2 = min(360 + player2.rotation, armRotation2 + 10)
-            //armRotation1 += 5
-        } else {
-            armRotation2 = 360 + player2.rotation
-        }
+        // if(armRotation2 < 360 + player2.rotation){
+        //     armRotation2 = min(360 + player2.rotation, armRotation2 + 10)
+        //     //armRotation1 += 5
+        // } else {
+        //     armRotation2 = 360 + player2.rotation
+        // }
 
-        backarm2.rotation = armRotation2
+        
 
     } else {
         
-        if(armRotation2 > 180 + player2.rotation){
-            armRotation2 = max(180 + player2.rotation, armRotation2 - 10)
-           //wwbackarm1.rotation -= 5
-        } else {
-            armRotation2 = armRotation2
-        }
+        // if(armRotation2 > 180 + player2.rotation){
+        //     armRotation2 = max(180 + player2.rotation, armRotation2 - 10)
+        //    //wwbackarm1.rotation -= 5
+        // } else {
+        //     armRotation2 = armRotation2
+        // }
 
-        backarm2.rotation = armRotation2
+        
+
+        //backarm2.rotation = armRotation2
         
         
         
         
     }
 
-    console.log(armRotation1)
     
     
     
@@ -197,10 +266,10 @@ function draw(){
         player1.rotationLock = true
         
         collisionFrame1 += 1;
-        if(ampl1 > 0){
+        if(ampl1 > 0.5){
             ampl1 -= 0.03
         } else {
-            ampl1 = 0
+            ampl1 = 0.5
         }
 
         // if(kb.presses('w')){
@@ -223,7 +292,7 @@ function draw(){
                 collisionFrame1 = Math.PI*15 
             }
             ampl1 = abs(player1.rotation/5)
-            ampl1 += min(0.1, max(2, ampl1)/50)
+            ampl1 += min(0.1, max(2, ampl1)/20)
         }
         //collisionFrame1 += 0.8
     }
@@ -235,10 +304,10 @@ function draw(){
         player2.vel.y = 0
         player2.rotationLock = true
         collisionFrame2 += 1;
-        if(ampl2 > 0){
+        if(ampl2 > 0.5){
             ampl2 -= 0.03
         } else {
-            ampl2 = 0
+            ampl2 = 0.5
         }
 
         // if(kb.presses('o')){
@@ -256,11 +325,13 @@ function draw(){
                 collisionFrame2 = Math.PI*15
             }
             ampl2 = abs(player2.rotation/5)
-            ampl2 += min(0.1, max(2, ampl2)/50)
+            ampl2 += min(0.1, max(2, ampl2)/20)
         } 
         
         //collisionFrame2 += 0.8
     }
+
+    
 
 
     player1.rotation = 5*ampl1*Math.cos(collisionFrame1/15)
